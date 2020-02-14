7cc800ff43221cf3188193602437bb81ef692d44
Miguel <mgimenez_sanchez@hotmail.com>
2020-02-14 11:58:03 +0100

    9u889

--- src/main/java/es/taes/App.java
+++ src/main/java/es/taes/App.java
@@ -1,9 +1,14 @@
 package es.taes;
 
 import java.sql.Statement;
+import java.util.Scanner;
+
+import com.mysql.fabric.xmlrpc.base.Value;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Hello world!
@@ -11,23 +16,16 @@ import java.sql.ResultSet;
  */
 public class App {
 
-    // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource";
-    static final String DB_URL = "jdbc:mysql://iprocuratio.com:3333/test_jdbc";
+  // JDBC driver name and database URL
+  static final String JDBC_DRIVER = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource";
+  static final String DB_URL = "jdbc:mysql://iprocuratio.com:3333/miguel";
 
-    // Database credentials
-    static final String USER = "root";
-    static final String PASS = "once012020";
+  // Database credentials
+  static final String USER = "root";
+  static final String PASS = "once012020";
 
-    public static void main(String[] args) throws Exception {
-        String miMensaje = "Mi hola mundo";
-        System.out.println(miMensaje);
-
-        Persona persona = new Persona();
-        persona.setEdad(45);
-        persona.setNombre("Alfonso");
-        System.out.println(persona);
-
+  public static void main(final String args[]) throws ClassNotFoundException, SQLException {
+    
         Connection conn = null;
         Statement stmt = null;
 
@@ -44,17 +42,41 @@ public class App {
         sql = "SELECT id, first, last, age FROM Employees";
         stmt.executeUpdate(
                 "CREATE TABLE if not exists Employees ( id INT(11) PRIMARY KEY, first VARCHAR(256),  last VARCHAR(256),age INTEGER)");
-        stmt.executeUpdate("INSERT ignore INTO Employees VALUES(1,'Jack','Smith', 100) ");
+                
+                final Scanner scan = new Scanner (System.in);
 
-        ResultSet rs = stmt.executeQuery(sql);
+          System.out.print("Pulsa 1 para crear un nuevo empleado u otra tecla para salir: ");
+          final String menu = scan.nextLine();
+
+              System.out.println(menu.length());
+              
+              if (menu.equals("1")) {
+
+                  System.out.print("Nombre del nuevo empleado: ");
+                  final String name = scan.nextLine();
+                  System.out.print("Apellido del nuevo empleado: ");
+                  final String sName = scan.nextLine();
+                  System.out.print("Edad del nuevo empleado: ");
+                  final String age = scan.nextLine();
+                
+                  String insert = "INSERT INTO  Employees (first, last, age) Values ('"+name+"', '"+sName+"', "+age+")";
+                  stmt.executeUpdate(insert);
+
+                        System.out.print("Empleado añadido");
+              
+              } else {
+              return;   
+              }
+          ;
+        final ResultSet rs = stmt.executeQuery(sql);
 
       // Extract data from result set
       while (rs.next()) {
         // Retrieve by column name
-        int id = rs.getInt("id");
-        int age = rs.getInt("age");
-        String first = rs.getString("first");
-        String last = rs.getString("last");
+        final int id = rs.getInt("id");
+        final int age = rs.getInt("age");
+        final String first = rs.getString("first");
+        final String last = rs.getString("last");
 
         System.out.print("ID: " + id);
         System.out.print(", Age: " + age);
@@ -67,4 +89,7 @@ public class App {
       conn.close();
 
     }
+
+  private static void Values(String name, String sName, Integer age) {
+  }
 }
